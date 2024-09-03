f47d8453b707c957c0ccaba9752d05c2bbaa4814
Emerson1911 <1284662@clases.edu.sv>
2024-09-03 11:39:09 -0600

    Terminado

---
+++ ENAE20230901AutenticationCooki/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ENAE20230901AutenticationCooki.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login(string login, string password)
+        {
+            if(login == "Emerson" && password == "Aguilar")
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,login),
+                };
+
+                var claimsIdentity = new ClaimsIdentity(claims,CookieAuthenticationDefaults.AuthenticationScheme);
+
+                var authProperties = new AuthenticationProperties
+                {
+
+                };
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+
+                return Ok("cuidame bien que lo mio es serio, que siempre me pierdo, quiero que estes, a mi lado otra vez");
+            }
+            else
+            {
+                return Unauthorized("Tengo reloj tengo reloj nuevo jeeee....");
+            }
+        }
+
+        [HttpPost("logout")]
+        public IActionResult Logout() { 
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return Ok("Perdiste reloj por lento..");
+        }
+    }
+}
---
+++ ENAE20230901AutenticationCooki/Controllers/NotasController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ENAE20230901AutenticationCooki.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class NotasController : ControllerBase
+    {
+        static List<object> data = new List<object>();
+        // GET: api/<NotasController>
+        [HttpGet]
+        [AllowAnonymous]
+        public IEnumerable<object> Get()
+        {
+            return data;
+        }
+
+
+        // POST api/<NotasController>
+        [HttpPost]
+        public IActionResult Post(string nombre, int nota)
+        {
+            data.Add(new {nombre, nota});
+
+            return Ok();
+        }
+
+       
+    }
+}
